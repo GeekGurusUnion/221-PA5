c7ce1dee8514c3684181a34dfcff7cd40215cdc6
ArrJayOh <rj.oosthuizen9@gmail.com>
2023-06-05 22:49:33 +0200

    Update filters.php
    Updates filters so Clients can view user table.

--- filters.php
+++ filters.php
@@ -5,8 +5,13 @@
 <h3>Select a table to view:</h3><br>
 <a href="./filter_wines.php" class="btn btn-primary" style="width:100px; margin-right: 10px">Wines</a>
 <a href="./filter_wineries.php" class="btn btn-primary" style="width:100px; margin-right: 10px">Wineries</a>
-<a href="./filter_users.php" class="btn btn-primary" style="width:100px; margin-right: 10px">Users</a>
+<a href="./filter_users.php" class="btn btn-primary" style="width:100px; margin-right: 10px" id="users">Users</a>
 <a href="./filter_reviews.php" class="btn btn-primary" style="width:100px">Reviews</a>
+<?php if ($_COOKIE['client'] == 'true') { ?>
+    <script>
+        document.getElementById("users").style.display = "none";
+    </script>
+<?php }  ?>
 
 <!-- </body> -->
 <?php include "footer.php";?>
