b23b17c08ae1755919829d65ce18ad3ba1605c8e
ArrJayOh <rj.oosthuizen9@gmail.com>
2023-06-07 10:36:17 +0200

    Update filters.php
    Edited so connoisseurs can't view the User table

--- filters.php
+++ filters.php
@@ -7,7 +7,7 @@
 <a href="./filter_wineries.php" class="btn btn-primary" style="width:100px; margin-right: 10px">Wineries</a>
 <a href="./filter_users.php" class="btn btn-primary" style="width:100px; margin-right: 10px" id="users">Users</a>
 <a href="./filter_reviews.php" class="btn btn-primary" style="width:100px">Reviews</a>
-<?php if ($_COOKIE['client'] == 'true') { ?>
+<?php if ($_COOKIE['client'] == 'true' || $_COOKIE['connoisseur'] == 'true') { ?>
     <script>
         document.getElementById("users").style.display = "none";
     </script>
